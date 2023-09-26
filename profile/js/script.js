4fd858de7ca7f7823158d23df27d9d4eba9172b7
pedrormedeiros <141637209+pedrormedeiros@users.noreply.github.com>
2023-09-26 11:48:30 -0300

    Delete profile directory

--- profile/js/script.js
+++
@@ -1,8 +0,0 @@
-function Enviarmensagem () {
-    var nome = document.getElementById("idNome");
-
-    if(nome.value != "") {
-        alert("Obrigado, sra(a)" + nome.value + ", sua mensagem foi enviada com sucesso!");
-
-    }
-}
